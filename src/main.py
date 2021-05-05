c4b1f95e9470c1b8cc6a00f51cab8d7f911320ed
griffin-walker <griffin.walker@mac.com>
2021-05-05 00:51:53 -0400

    Throw dask at it

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
 from prefect import Flow
-from query import get_training_data, pretend_connection
+from prefect.executors import LocalDaskExecutor
+from query import get_initial_data, get_secondary_data, get_tertiary_data, pretend_connection
 
 with Flow('example') as flow:
-    X_train, y_train = get_training_data(conn = pretend_connection())
+    flow.executor = LocalDaskExecutor()
+    conn = pretend_connection()
+    X_initial, y_initial = get_initial_data(conn)
+    X_secondary, y_secondary = get_secondary_data(conn)
+    X_tertiary, y_tertiary = get_tertiary_data(conn)
     flow.run()
--- src/query.py
+++ src/query.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import make_classification
-
+from time import sleep
 from prefect import task, context
+
 
 data_params = {
     "n_samples" : 10000,
@@ -16,8 +17,27 @@ def pretend_connection():
     return conn
 
 @task(nout=2)
-def get_training_data(conn, data_params=data_params):
+def get_initial_data(conn, data_params=data_params):
+    sleep(5)
     logger = context.get("logger")
+    assert not conn
+    X, y = make_classification(**data_params)
+    logger.info(f"X shape : {X.shape}, y shape : {y.shape}")
+    return X, y
+
+@task(nout=2)
+def get_secondary_data(conn, data_params=data_params):
+    sleep(5)
+    logger = context.get("logger")
+    assert not conn
+    X, y = make_classification(**data_params)
+    logger.info(f"X shape : {X.shape}, y shape : {y.shape}")
+    return X, y
+
+@task(nout=2)
+def get_tertiary_data(conn, data_params=data_params):
+    logger = context.get("logger")
+    sleep(5)
     assert not conn
     X, y = make_classification(**data_params)
     logger.info(f"X shape : {X.shape}, y shape : {y.shape}")
